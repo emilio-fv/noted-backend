fc069f46c2edee315e58228fee88f70eff48a417
Emilio Vazquez <emiliofv.sd@gmail.com>
2023-11-15 13:11:57 -0700

    fix logger error

--- utils/logger.util.js
+++ utils/logger.util.js
@@ -1,7 +1,7 @@
 // Imports
 const { createLogger, transports, format } = require('winston');
 
-const logger = winston.createLogger({
+const logger = createLogger({
     format: format.combine(
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss'}),
         format.errors({ stack: true }),
