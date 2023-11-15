ae7447c61443bcc6df092a5c4d4428146bab0081
Emilio Vazquez <emiliofv.sd@gmail.com>
2023-11-15 12:54:45 -0700

    update docker compose test file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 const { connectDb } = require('./config/mongoose.config');
 const { seedDb } = require('./seed');
 const { authRouter } = require('./api/auth/auth.routes');
