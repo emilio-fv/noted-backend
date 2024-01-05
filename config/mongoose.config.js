f5d71e2ab8e31702695a5447b5088ab06aa1f5c4
Emilio Vazquez <109986999+emilio-fv@users.noreply.github.com>
2024-01-04 22:25:38 -0700

    Update mongoose.config.js

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,15 +1,11 @@
 // Imports
 const mongoose = require('mongoose');
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@cluster0.phxrehb.mongodb.net/?retryWrites=true&w=majority`;
-console.log(MONGODB_URI);
-
 // Connect to mongodb 
 const connectDb = async () => {
     try {
-        await mongoose.connect(MONGODB_URI).then(() => {
-            console.log('Connected to db successfully');
-        })
+        const mongodbConnection = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Mongo db connected: ${mongodbConnection.connection.host}`);
     } catch (error) {
         // TODO: handle error
         console.log(error);
